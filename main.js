76d1057370c9a03105965ccc2c229c679100794e
Ryan Phillips <wordsandcolor@gmail.com>
2015-03-29 16:59:08 -0700

    first img
    generated an image from the first midifile via canvas shapes

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
-var fs = require('fs');
-var midiConverter = require('midi-converter');
-var midiSong = fs.readFileSync('20150327.mid', 'binary');
-var jsonSong = midiConverter.midiToJson(midiSong);
-fs.writeFileSync('20150327.json', JSON.stringify(jsonSong));
+
+var track1;
+
+exports.foo = function() {
+	var fs = require('fs');
+	var midiConverter = require('midi-converter');
+	var midiSong = fs.readFileSync('20150327.mid', 'binary');
+	var jsonSong = midiConverter.midiToJson(midiSong);
+	//fs.writeFileSync('20150327.json', JSON.stringify(jsonSong)); //save a copy
+	var jsonSong_data = require('./20150327.json');
+	track1 = jsonSong_data.tracks[1]
+	console.log('done');
+};
